1df21f6afc85e68599f9284ebc791400e63d02d5
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-09-29 22:49:21 +0000

    Added randomString function and post request code

--- express_server.js
+++ express_server.js
@@ -1,12 +1,19 @@
-const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
+const express       = require("express");
+const app           = express();
+const PORT          = 8080; // default port 8080
+const bodyParser    = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+//Function to generate a random string
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(2,8);
 };
 
 app.get("/", (req, res) => {
@@ -26,9 +33,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL =  urlDatabase[shortURL].longURL;
   const templateVars = { shortURL, longURL};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
