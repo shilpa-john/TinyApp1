27c8f1f6d967355d4b61969138f0e3aa602121ab
Shilpa Chegudi <shilpachegudi@gmail.com>
2020-10-02 01:00:35 +0000

    Added routes for login & logout,installed Cookie code too!

--- express_server.js
+++ express_server.js
@@ -2,9 +2,12 @@ const express       = require("express");
 const app           = express();
 const PORT          = 8080; // default port 8080
 const bodyParser    = require("body-parser");
+var   cookieParser  = require('cookie-parser');
+
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,27 +23,35 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//Route for Login And entering the Username
+app.post("/login", (req, res) => {
+  username = req.body.username;
+  res.cookie("username", username).redirect("/urls");
+
+});
+
+//For urls display
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"],
+    urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const longURL =  urlDatabase[shortURL].longURL;
-  const templateVars = { shortURL, longURL};
+  const longURL =  urlDatabase[shortURL];
+  const templateVars = { shortURL, longURL, username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 
@@ -51,7 +62,30 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.post("/urls", (req, res) => {
+//Delete URL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
+/*app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});*/
+
+// Login-Authentication
+/*app.get("/login", (req, res) => {
+  let templateVars = { urls: urlDatabase, username: req.cookies.username};
+  res.render("urls_index", templateVars);
+});*/
+
+//Clear the Cookie on logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("username").redirect("/urls")
+});
+
+//Connecting To The Server!
+app.listen(PORT, () => {
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
